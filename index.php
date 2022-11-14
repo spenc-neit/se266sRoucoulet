7037823de567d09b12d56d02d7b388bf8cf221e6
Spencer Roucoulet <sdroucoulet@email.neit.edu>
2022-11-13 19:45:30 -0500

    entire push for one tiny fix :/

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         <li><a href="week1/">Week 1 - Mini Tasks C-G</a></li>
         <li><a href="week2/b/indexfront.php">Week 2 - Patient Intake Form</a></li>
         <li><a href="week3/atm_starter.php">Week 3 - ATM Simulation</a></li>
-        <li><a href="#">Week 4</a></li>
+        <li><a href="week4">Week 4</a></li>
         <li><a href="#">Week 5</a></li>
         <li><a href="#">Week 6</a></li>
         <li><a href="#">Week 7</a></li>
